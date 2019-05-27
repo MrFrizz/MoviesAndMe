83efe7c5ee0f67c42bcc339874d824f317c96cac
Louis Mantopoulos <Louis@macbook-pro-de-louis-2.home>
2019-05-27 22:53:17 +0200

    first commit

---
+++ Components/Search.js
@@ -0,0 +1,92 @@
+import React from 'react'
+import { StyleSheet, View, Button, TextInput, FlatList, Text, ActivityIndicator } from 'react-native'
+import FilmItem from './FilmItem'
+import { getFilmsFromApiWithSearchedText } from '../API/TMDBApi'
+
+class Search extends React.Component {
+
+  constructor(props) {
+
+    super(props)
+    this.state = {
+      films: [],
+      isLoading: false
+    }
+    this.searchedText = ""
+  }
+
+  _loadFilms() {
+    this.setState({isLoading: true})
+    if (this.searchedText.length > 0) {
+      getFilmsFromApiWithSearchedText(this.searchedText).then(data =>
+        this.setState({
+          films: data.results,
+          isLoading: false}
+        )
+      )
+    }
+  }
+
+  _displayLoading() {
+    if (this.state.isLoading) {
+      return (
+        <View style={ styles.loading_container }>
+          <ActivityIndicator size='large'/>
+        </View>
+      )
+    }
+  }
+
+  _searchTextInputChanged(text) {
+    this.searchedText = text
+  }
+
+  // _searchTextInputChanged(text) {
+  //   if (text.length > 2) {
+  //     getFilmsFromApiWithSearchedText(text).then(data => this.setState({films: data.results}))
+  //   }
+  // }
+
+  render() {
+    return (
+      <View style={ styles.mainContainer }>
+        <TextInput onSubmitEditing={() => this._loadFilms()} onChangeText={(text) => this._searchTextInputChanged(text)} style={ styles.textinput } placeholder="Titre du film"/>
+        <Button style={{ flex: 1, height: 50 }} title="Rechercher" onPress={() => this._loadFilms()}/>
+        <FlatList
+          data={this.state.films}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({item}) => <FilmItem film={item}/>}
+        />
+        {this._displayLoading()}
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    marginTop: 20,
+    flex: 1
+  },
+
+  textinput: {
+    marginLeft: 5,
+    marginRight: 5,
+    height: 50,
+    borderColor: '#000000',
+    borderWidth: 1,
+    paddingLeft: 5
+  },
+
+  loading_container: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 100,
+    bottom: 0,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
+
+export default Search
